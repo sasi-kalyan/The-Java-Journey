4fbbe577400aead5de610ef68fb49c020f34226e
sasi-kalyan <kanakamsasikalyan1@gmail.com>
2026-09-01 18:26:09 +0530

    spring-mvc pattern

--- spring-framework/spring-mvc-pattern/src/main/java/io/mvc/pattern/demo/DemoApplication.java
+++ spring-framework/spring-mvc-pattern/src/main/java/io/mvc/pattern/demo/DemoApplication.java
@@ -8,6 +8,37 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class DemoApplication {
 
+    /*
+            Spring MVC is a web framework that is built on the top of the MVC framework that
+            uses the Front controller architecture to handle the http request effectiently.
+
+            SingleFrontController = DispatcherServlet
+
+            Browser -> DispatcherServlet -> Controller -> Service -> Repository -> Database
+
+            Heart of the SpringMVC:
+            DispatcherServlet is the Front Controller of Spring MVC that receives every request and delegates or maps
+            it to the appropriate controller.
+
+            Controller:
+            	- Handles HTTP request/respose
+            	- Path variables
+            	- Request parms
+
+            Service:
+            	BusinessLogic
+            	Rules
+            	Calculations
+            	Validations
+            	Transactions
+
+            Repository:
+            	DB operations
+            	CRUD
+            	Queries
+
+     */
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
